91819088b2ceeaf50e3a56f406f06faae4c6bd8e
Finn Harms <post@finn-harms.de>
2023-05-18 01:26:01 +0200

    fix

--- whiteboardbot/main.py
+++ whiteboardbot/main.py
@@ -3,6 +3,6 @@ import time
 
 def run_bot():
     for i in range(100):
-        motorshield.set_motor(i/100, 0)
-        motorshield.set_motor(1-(i/100), 1)
+        motorshield.set_motor(0, i/100)
+        motorshield.set_motor(1, 1-(i/100))
         time.sleep(0.01)
