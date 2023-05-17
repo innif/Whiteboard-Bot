5079e4ce05eaef19ef94cb4d5dee06482eb5e6b0
Finn Harms <post@finn-harms.de>
2023-05-18 01:46:31 +0200

    extended example

--- whiteboardbot/main.py
+++ whiteboardbot/main.py
@@ -2,11 +2,12 @@ from . import motorshield
 import time
 
 def run_bot():
-    for i in range(100):
-        motorshield.set_motor(0, i/100)
-        motorshield.set_motor(1, 1-(i/100))
-        time.sleep(0.01)
+    while True:
+        for i in range(-100, 100, 1):
+            motorshield.set_motor(0, i/100)
+            motorshield.set_motor(1, 1-(i/100))
+            time.sleep(0.01)
 
-    
-    motorshield.set_motor(0, 0)
-    motorshield.set_motor(1, 0)
+        motorshield.set_motor(0, 0)
+        motorshield.set_motor(1, 0)
+        time.sleep(2)
