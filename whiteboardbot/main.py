4b2775e39f8378cd5b2dbd82702e1135b924ca38
Finn Harms <post@finn-harms.de>
2023-05-18 02:16:54 +0200

    fix

--- whiteboardbot/main.py
+++ whiteboardbot/main.py
@@ -12,7 +12,7 @@ def run_bot():
     @app.route('/motor/<id>/<power>') 
     def handle_request(id: str, power: str):
         id = int(id)
-        power = int(id)
+        power = int(power)/100
         print(f"Motor {id} Power {power}")
         motorshield.set_motor(id, power)
         return "OK 200"   
