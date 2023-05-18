1264b83d973cb625a64b81eab736e7072fbfd8db
Finn Harms <post@finn-harms.de>
2023-05-18 02:40:49 +0200

    test

--- whiteboardbot/main.py
+++ whiteboardbot/main.py
@@ -10,11 +10,15 @@ def run_bot():
         return "Hey!"   
         
     @app.route('/motor/<id>/<power>') 
-    def handle_request(id: str, power: str):
-        id = int(id)
+    def handle_request(motor_id: str, power: str):
+        motor_id = int(motor_id)
         power = int(power)/100
-        print(f"Motor {id} Power {power}")
-        motorshield.set_motor(id, power)
+        print(f"Motor {motor_id} Power {power}")
+        motorshield.set_motor(motor_id, power)
         return "OK 200"   
-                                
+
+    
+    motorshield.set_motor(1, 1)
+    time.sleep(2)
+    motorshield.set_motor(1, 0)                      
     app.run(debug=True, port=5000, host='0.0.0.0',threaded=True)
