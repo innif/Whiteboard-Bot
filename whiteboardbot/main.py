03512e15399c19d16afe98b6a8b11d6c46bd408e
Finn Harms <post@finn-harms.de>
2023-05-18 03:59:31 +0200

    fix

--- whiteboardbot/main.py
+++ whiteboardbot/main.py
@@ -10,7 +10,7 @@ def run_bot():
         return "Hey!"   
         
     @app.route('/motor/<motor_id>/<power>') 
-    def handle_request(motor_id: str, power: str):
+    def handle_motor_request(motor_id: str, power: str):
         motor_id = int(motor_id)
         power = int(power)/100
         print(f"Motor {motor_id} Power {power}")
@@ -18,7 +18,7 @@ def run_bot():
         return "OK 200"   
     
     @app.route('/steer/<m1_power>/<m2_power>') 
-    def handle_request(m1_power: str, m2_power: str):
+    def handle_steer_request(m1_power: str, m2_power: str):
         m1_power = int(m1_power)
         m2_power = int(m2_power)
         motorshield.set_motor(0, m1_power)
